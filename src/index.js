12b0f0c143bbba27e6dc923b93ccf77d04bd0b5e
Gavin <1490259042@qq.com>
2022-03-13 13:59:33 +0800

    feat: add commit hook

--- src/index.js
+++ src/index.js
@@ -1,63 +1,50 @@
 function maxLength(dom) {
-  let max = 10
+  let max = 10;
   let meta = {
-    innerText: '',
-    innerHTML: ''
-  }
+    innerText: "",
+    innerHTML: "",
+  };
 
-  let fragment
+  let fragment;
 
-  // dom.addEventListener('input', (e) => {
-  //   console.log(e)
-
-  //   // e.preventdefault()
-  //   e.data = ''
-  //   e.returnValue = false
-  // })
-
-  dom.addEventListener('keydown',e=>{
-    if(e.target.innerText.length >= 10 && e.keyCode !== 8){
-        e.preventDefault();
+  dom.addEventListener("keydown", (e) => {
+    if (e.target.innerText.length >= 10 && e.keyCode !== 8) {
+      e.preventDefault();
     }
-  })
+  });
 
-  dom.addEventListener('compositionstart', e => {
-    // console.log(e, 'start')
-    meta.innerHTML = dom.innerHTML
-    meta.innerText = dom.innerText.replace(/\r|\n|(\r\n)/g, '') // 去掉换行
-  })
+  dom.addEventListener("compositionstart", (e) => {
+    meta.innerHTML = dom.innerHTML;
+    meta.innerText = dom.innerText.replace(/\r|\n|(\r\n)/g, ""); // 去掉换行
+  });
 
-  dom.addEventListener('compositionupdate', e => {
-    // console.log(e, 'update')
+  dom.addEventListener("compositionupdate", (e) => {
+    e.preventDefault();
+  });
 
-    e.preventDefault()
-  })
-
-  dom.addEventListener('compositionend', e => {
-    const diff =  max - meta.innerText.length
-    // console.log('diff:', diff, 'innerText:', meta.innerText.length)
+  dom.addEventListener("compositionend", (e) => {
+    const diff = max - meta.innerText.length;
 
     // 证明输入的多了，要截取然后插入
     if (diff < e.data.length) {
-      const selection = window.getSelection()
-      const range = selection.getRangeAt(0)
+      const selection = window.getSelection();
+      const range = selection.getRangeAt(0);
 
-      const data = e.data.slice(0, diff)
-      const result = meta.innerHTML + data
+      const data = e.data.slice(0, diff);
+      const result = meta.innerHTML + data;
 
-      fragment = document.createDocumentFragment()
+      fragment = document.createDocumentFragment();
 
-      dom.innerHTML = ''
+      dom.innerHTML = "";
 
-      fragment.append(result)
+      fragment.append(result);
 
-      range.insertNode(fragment)
+      range.insertNode(fragment);
 
-      range.collapse()
+      range.collapse();
     }
-  })
+  });
 }
 
-
-let dom = document.getElementById('con')
-maxLength(dom)
+let dom = document.getElementById("con");
+maxLength(dom);
